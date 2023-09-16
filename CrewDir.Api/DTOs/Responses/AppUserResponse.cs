8b35c9e89e9ce34b043832f9413f02be0904d71f
Adam Flett <Adam@adamflett.net>
2023-09-16 22:01:53 +0800

    fixed nulls

--- CrewDir.Api/DTOs/Responses/AppUserResponse.cs
+++ CrewDir.Api/DTOs/Responses/AppUserResponse.cs
@@ -2,8 +2,8 @@
 {
     public class AppUserResponse
     {
-        public string Id { get; set; }
-        public string Email { get; set; }
+        public string Id { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
 
         public EmployeeResponse? Employee { get; set; }
     }
